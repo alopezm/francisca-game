6be6e56dcc370603fea13f675f4bc6818cb8d44e
Francisca <mjaramillo9403@gmail.com>
2024-09-01 01:13:46 -0500

    add berlin divisions 1 scene_organized enemys

--- components/Game/scenes/BerlinScene/BerlinScene.buildings.js
+++ components/Game/scenes/BerlinScene/BerlinScene.buildings.js
@@ -29,33 +29,145 @@ export const BUILDINGS = [
     key: "/assets/water.png",
     items: [
       {
-        setXY: { x: 0, y: 1117 }
+        setXY: { x: 1500, y: 0 }
       }
     ]
   },
   {
-    key: "/assets/water.png",
+    key: "/assets/water2.png",
     items: [
       {
-        setXY: { x: 1044, y: 1111 }
+        setXY: { x: 1500, y: 1047 }
       }
     ]
   },
   {
-    key: "/assets/house_line1_mini",
+    key: "/assets/house_line1_mini.png",
     items: [
       {
-        setXY: { x: 0, y: 2819 }
+        setXY: { x: -4, y: 2724 }
       }
     ]
   },
   {
-    key: "/assets/house_line2_mini",
+    key: "/assets/berlin_division6_mini.png",
     items: [
       {
-        setXY: { x: 2819, y: 900 }
+        setXY: { x: 910, y: 2760 }
+      }
+    ]
+    },
+  {
+    key: "/assets/house_line2_mini.png",
+    items: [
+      {
+        setXY: { x: 1147, y: 2724 }
       }
     ]
   },
-
+  {
+    key: "/assets/house_line3_mini.png",
+    items: [
+      {
+        setXY: { x: 0, y: 1992 }
+      }
+    ]
+  },
+  {
+    key: "/assets/house_line6_mini.png",
+    items: [
+      {
+        setXY: { x: 136, y: 2145 }
+      }
+    ]
+  },
+  {
+    key: "/assets/berlin_division1_mini.png",
+    items: [
+      {
+        setXY: { x: 140, y: 2500 }
+      }
+    ]
+  },
+  {
+    key: "/assets/home_divion_side_mini.png",
+    items: [
+      {
+        setXY: { x: 1500, y: 1500 }
+      }
+    ]
+  },
+  {
+    key: "/assets/house_line4_mini.png",
+    items: [
+      {
+        setXY: { x: 1010, y: 1750 }
+      }
+    ]
+  },
+  {
+    key: "/assets/house_berlin2_mini.png",
+    items: [
+      {
+        setXY: { x: 978, y: 2288 }
+      }
+    ]
+  },
+  {
+    key: "/assets/berlin_division2_mini.png",
+    items: [
+      {
+        setXY: { x: 463, y: 2332 }
+      }
+    ]
+  },
+  {
+    key: "/assets/berlin_division5_mini.png",
+    items: [
+      {
+        setXY: { x: 1270, y: 1835 }
+      }
+    ]
+  },
+  {
+    key: "/assets/house_line7_mini.png",
+    items: [
+      {
+        setXY: { x: 1250, y: 1897 }
+      }
+    ]
+  },
+  {
+    key: "/assets/berlin_division3_mini.png",
+    items: [
+      {
+        setXY: { x: 515, y: 2050 }
+      }
+    ]
+  },
+  {
+    key: "/assets/berlin_division4_mini.png",
+    items: [
+      {
+        setXY: { x: 493, y: 1732 }
+      }
+    ]
+  },
+  {
+    key: "/assets/house_line5_mini.png",
+    items: [
+      {
+        setXY: { x: 450, y: 1412 }
+      }
+    ]
+  },
+  {
+    key: "/assets/House_berlin1_mini.png",
+    items: [
+      {
+        setXY: { x: 0, y: 1412 }
+      }
+    ]
+  },
+  
 ];
--- components/Game/scenes/BerlinScene/BerlinScene.enemies.js
+++ components/Game/scenes/BerlinScene/BerlinScene.enemies.js
@@ -1,9 +1,9 @@
 export const ENEMIES = [
   {
-    x: 900,
-    y: 1800,
-    velocityX: -50,
-    velocityY: 0,
+    x: 760,
+    y: 2750,
+    velocityX: 0,
+    velocityY: 50,
   },
   {
     x: 1100,
@@ -12,21 +12,22 @@ export const ENEMIES = [
     velocityY: 0,
   },
   {
-    x: 1000,
-    y: 1700,
-    velocityX: 0,
-    velocityY: -50,
+    x: 550,
+    y: 1675,
+    velocityX: -50,
+    velocityY: 0,
   },
   {
-    x: 1000,
-    y: 1900,
+    x: 750,
+    y: 290,
+    velocityX: -50,
+    velocityY: 0,
+  },
+  {
+    x: 100,
+    y: 900,
     velocityX: 0,
     velocityY: 50,
   },
-  {
-    x: 1000,
-    y: 1800,
-    velocityX: 0,
-    velocityY: 0,
-  },
+
 ];

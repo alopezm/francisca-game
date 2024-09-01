cc4454282cae118fcff3e710e3607a682050f542
Francisca <mjaramillo9403@gmail.com>
2024-09-01 01:23:44 -0500

    first positions

--- components/Game/scenes/BerlinScene/BerlinScene.config.js
+++ components/Game/scenes/BerlinScene/BerlinScene.config.js
@@ -1,9 +1,9 @@
-// TODO: remove or restore to 1
+//TODO: remove or restore to 1
 export const SCENE_ZOOM = 1;
-// export const SCENE_ZOOM = 0.5;
+//export const SCENE_ZOOM = 0.5;
 
 export const PLAYER_VELOCITY = 190;
 
-export const PLAYER_X = 600;
+export const PLAYER_X = 800;
 
-export const PLAYER_Y = 1200;
+export const PLAYER_Y = 3000;

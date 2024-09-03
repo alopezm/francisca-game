8176f31d526fae536bf99acb938caffab0942879
Anderson LM <anderson1564@gmail.com>
2024-09-03 11:51:51 -0500

    Update credits texts

--- components/Game/scenes/CreditsScene.js
+++ components/Game/scenes/CreditsScene.js
@@ -16,10 +16,10 @@ export class CreditsScene extends BaseScene {
     };
 
     this.add.text(100, 300, "Diseño:", style);
-    this.add.text(100, 370, "Francisca Jaramillo", style);
+    this.add.text(100, 370, "Francisca Jaramillo y Anderson López", style);
 
     this.add.text(100, 500, "Desarrollo:", style);
-    this.add.text(100, 570, "Anderson", style);
+    this.add.text(100, 570, "Anderson López", style);
 
     this.add.text(100, 700, "Guión e historia:", style);
     this.add.text(100, 770, "Francisca Jaramillo y Felipe Marín", style);

3bd777da098626931d058f8ee9e8b9f3069b78e4
Francisca <mjaramillo9403@gmail.com>
2024-09-09 23:57:45 -0500

    first Investigación, guió e historia in credits

--- components/Game/scenes/CreditsScene.js
+++ components/Game/scenes/CreditsScene.js
@@ -19,10 +19,10 @@ export class CreditsScene extends BaseScene {
     this.add.text(100, 770, "Francisca Jaramillo y Felipe Marín", style);
   
     this.add.text(100, 300, "Diseño:", style);
-    this.add.text(100, 370, "Francisca Jaramillo y Anderson López", style);
+    this.add.text(100, 370, "Ma. Francisca Jaramillo y Anderson López", style);
 
     this.add.text(100, 500, "Desarrollo:", style);
-    this.add.text(100, 570, "Anderson López y Francisca Jaramillo", style);
+    this.add.text(100, 570, "Anderson López y Ma. Francisca Jaramillo", style);
   
   }
 }
